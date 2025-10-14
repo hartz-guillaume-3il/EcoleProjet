f2849cf1e7c06731352927921a0d85f39b8179a6
hartz-guillaume-3il <hartzg@3il.fr>
2025-10-14 17:55:57 +0200

    Update AffectationPremierArrive.java

--- src/strategy/affectation/AffectationPremierArrive.java
+++ src/strategy/affectation/AffectationPremierArrive.java
@@ -4,9 +4,7 @@ import state.Creneau;
 
 import java.util.List;
 
-/**
- * Premier arrivé, premier servi: on prend le premier créneau encore ouvert.
- */
+
 public final class AffectationPremierArrive implements StrategieAffectation {
     @Override
     public Creneau choisir(Candidat c, List<Creneau> cs) {
